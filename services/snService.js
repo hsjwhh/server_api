bba56df512401cca086dabb4f91dbdea9f321fbc
💻Debian12 <vm@host.com>
2026-02-02 21:07:25 +0800

    更新 sql

--- services/snService.js
+++ services/snService.js
@@ -4,10 +4,11 @@ const { query } = require("../db");
 // ✅ 搜索 SN
 exports.search = async (keyword) => {
     const rows = await query(
-        "SELECT SN FROM  server_info WHERE SN LIKE ? LIMIT 20",
+        "SELECT `业务`,`出机客户`,`年份`,`月份`,`日期`,SN FROM  server_info WHERE SN LIKE ? LIMIT 20",
         [`%${keyword}%`]
     );
-    return rows.map(r => r.SN);
+    // return rows.map(r => r.SN);
+    return rows;
 };
 
 // ✅ 根据 SN 获取详情
